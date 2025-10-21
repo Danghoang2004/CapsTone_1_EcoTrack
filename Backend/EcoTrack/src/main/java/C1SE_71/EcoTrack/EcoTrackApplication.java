6bae6d5dd80c5ecec18997318954ab217220cdcc
Dang Van Hoang <dangvanhoang181004@gmail.com>
2025-10-22 00:03:05 +0700

    Initial commit - Ecotrack

---
+++ Backend/EcoTrack/src/main/java/C1SE_71/EcoTrack/EcoTrackApplication.java
@@ -0,0 +1,13 @@
+package C1SE_71.EcoTrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoTrackApplication.class, args);
+	}
+
+}
---
+++ Backend/EcoTrack/src/test/java/C1SE_71/EcoTrack/EcoTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package C1SE_71.EcoTrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
